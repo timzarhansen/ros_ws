b426cde73a26d9256b67830fd2e935c8bf7fffb5
timhansen <timhansen93@googlemail.com>
2025-02-18 13:55:18 +0100

    update on deep test

--- configFiles/installros.sh
+++ configFiles/installros.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sudo chown -R tim-external /home/tim-external/ros_ws
+source /home/tim-external/.bashrc
 cd /home/tim-external/ros_ws/
 colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release
