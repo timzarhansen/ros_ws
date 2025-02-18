1df174386a5dbd636239784b3465ff896ab30d2c
timhansen <timhansen93@googlemail.com>
2025-02-18 14:03:38 +0100

    update on deep test

--- configFiles/installros.sh
+++ configFiles/installros.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+source /opt/ros/humble/setup.bash
+source /home/tim-external/ros_ws/install/setup.bash
 sudo chown -R tim-external /home/tim-external/ros_ws
 source /home/tim-external/.bashrc
 cd /home/tim-external/ros_ws/
