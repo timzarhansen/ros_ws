88b1db5fae08dccab5f9c23815d542a86aca180d
timhansen <timhansen93@googlemail.com>
2025-02-18 13:29:21 +0100

    update on deep test

--- configFiles/installros.sh
+++ configFiles/installros.sh
@@ -1,22 +1,4 @@
 #!/bin/bash
-
-#sudo rm -r ros_ws/
-#git clone https://github.com/timzarhansen/ros_ws.git
-#cd ros_ws/
-#git submodule update --init --recursive
-#
-#docker run -t -i --rm --ipc=host -v /home/deeprobotics/ros_ws/cache/humble/build:/home/tim-external/ros_ws/build -v /home/deeprobotics/ros_ws/cache/humble/install:/home/tim-external/ros_ws/install -v /home/deeprobotics/ros_ws/cache/humble/log:/home/tim-external/ros_ws/log -v /home/deeprobotics/ros_ws/configFiles:/home/tim-external/ros_ws/configFiles -v /home/deeprobotics/ros_ws/src:/home/tim-external/ros_ws/src -v /home/deeprobotics/dataFolder:/home/tim-external/dataFolder computationimageodometryamd
-#docker run -t -i --rm --ipc=host -v /home/deeprobotics/ros_ws/cache/humble/build:/home/tim-external/ros_ws/build -v /home/deeprobotics/ros_ws/cache/humble/install:/home/tim-external/ros_ws/install -v /home/deeprobotics/ros_ws/cache/humble/log:/home/tim-external/ros_ws/log -v /home/deeprobotics/ros_ws/configFiles:/home/tim-external/ros_ws/configFiles -v /home/deeprobotics/ros_ws/src:/home/tim-external/ros_ws/src -v /home/deeprobotics/dataFolder:/home/tim-external/dataFolder computationimageodometryamd
 sudo chown -R tim-external /home/tim-external/ros_ws
 cd /home/tim-external/ros_ws/
 colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release
-exit
-
-
-
-
-
-
-
-
-
