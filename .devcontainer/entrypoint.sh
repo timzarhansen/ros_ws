c30b9e71864f9e2c162e352b9510e30e587447b7
timhansen <timhansen93@googlemail.com>
2026-05-22 11:57:33 +0200

    updated Human

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-# Source conda so 'conda activate ml' works (but conda is not on PATH)
-if [ -f /opt/miniforge3/etc/profile.d/conda.sh ]; then
-    . /opt/miniforge3/etc/profile.d/conda.sh
-fi
-
 # Source ROS 2 and workspace setup
 source /opt/ros/jazzy/setup.bash
 [ -f /home/tim-external/ros_ws/install/setup.bash ] && source /home/tim-external/ros_ws/install/setup.bash
