7883a299abc8ba70360e79eec40d1e042dba6d2e
timhansen <timhansen93@googlemail.com>
2026-05-26 13:11:56 +0200

    added full testrun tests

--- .benchmark_docker/full_test_run.sh
+++ .benchmark_docker/full_test_run.sh
@@ -3,12 +3,8 @@ set -euo pipefail
 bash .benchmark_docker/cleanup_before_benchmark.sh
 git stash
 git pull
-cd src/fsregistration
-git stash
-cd ..
-cd ..
-cd ..
-sudo chmod -R 777 ros_ws/
+git -C src/fsregistration stash
+sudo chmod -R 777 .
 cd ros_ws/
 bash .benchmark_docker/run_test.sh
 
